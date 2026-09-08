38695078da72d92f4b0a316ff71f8cdea93851f1
njlyon0 <nicholasjlyon@gmail.com>
2026-09-08 14:23:44 -0400

    feat (down): identify cleaner, algorithmic download of entities

--- 00_download-and.r
+++ 00_download-and.r
@@ -6,7 +6,7 @@
 
 # Load libraries
 # install.packages("librarian")
-librarian::shelf(EDIutils, tidyverse)
+librarian::shelf(EDIutils, tidyverse, tools)
 
 # Get set up
 source(file.path("-setup.r"))
@@ -14,7 +14,7 @@ source(file.path("-setup.r"))
 # Clear environment/collect garbage
 rm(list = ls()); gc()
 
-# Define the 3-letter site abbreviation
+# Define 3-letter site abbreviation
 site_abbrev <- "AND"
 
 ## -------------------------------------------- ##
@@ -25,43 +25,61 @@ site_abbrev <- "AND"
 ## YouTube Tutorial -- < https://youtu.be/fieZSmHk2H4?si=Wo9a5GsAOYp3dnWS >
 ## Identity & Access Manager (IAM) -- < https://auth.edirepository.org >
 
-## -------------------------------------------- ##
-# Download Data ----
-## -------------------------------------------- ##
 # Once you have a key,
 ## 1. Create a file in the top-level of this folder called "secret_my-edi-key.md"
 ### DO NOT COMMIT THIS FILE! 
 ### The above name has been preemptively added to the `.gitignore` but if you name it something else, you'll be at risk of committing it
 ## 2. Copy/paste it into the "Console" of your IDE when prompted by the following code
 
-# Identify URL(s) for data entity/entities of interest
-for(focal_url in c(
-  "https://pasta.lternet.edu/package/data/eml/knb-lter-and/4032/10/aed12b7432db4b68e0e97f7ff6ad24b1"
-  )){
+# Define your EDI key
+(edi_key <- readline(prompt = "Copy/paste your EDI Access Key here: "))
 
-  # Define your EDI key
-  (edi_key <- readline(prompt = "Copy/paste your EDI Access Key here: "))
+# Log in with that key
+EDIutils::login(key = edi_key)
 
-  # If no key is found, error out
-  if("edi_key" %in% ls() != TRUE){
-    stop("A valid EDI Access Key is required for this code to download the data!")
+# Set HTTP option
+options(HTTPUserAgent = "EDI_CodeGen")
 
-    # Otherwise, use it to download the relevant data file
-  } else {
-    # Set HTTP option
-    options(HTTPUserAgent = "EDI_CodeGen")
+## -------------------------------------------- ##
+# Download Data ----
+## -------------------------------------------- ##
 
-    # Assemble link
-    in_url <- paste0(focal_url, "?key=", edi_key) 
+# Define package ID
+pkg_id <- "knb-lter-and.4032.10"
 
-    # Assemble local file name
-    in_file <- file.path("data", "raw",
-      paste0("raw_", site_abbrev, ".csv"))
+# Check out data
+(ents <- EDIutils::read_data_entity_names(packageId = pkg_id))
 
-    # Download it!
-    download.file(url = in_url, destfile =  in_file,
-      method = "curl", 
-      extra = paste0(' -A "', getOption("HTTPUserAgent"), '"'))
-  } }
+# Loop across entities
+for(k in seq_along(ents$entityName)){
+  # k <- 2
+
+  # Grab just that entity
+  focal_ent <- ents[k, ]
+
+  # Progress message
+  message("Downloading file ", k, " of ", nrow(ents))
+  
+  # Assemble URL to that entity
+  in_url <- paste0("https://pasta.lternet.edu/package/data/eml/",
+    gsub(pattern = "\\.", "/", x = pkg_id), "/",
+    focal_ent$entityId, "?key=", edi_key)
+
+  # Identify entity file type
+  ent_type <- tools::file_ext(focal_ent$entityName)
+  
+  # If unidentified, assume CSV
+  if(nchar(ent_type) == 0 | is.na(ent_type)){
+    focal_ent$entityName <- paste0(focal_ent$entityName, ".csv")
+  }
+  
+  # Assemble local file name
+  in_file <- file.path("data", "raw",
+    paste0("00_", site_abbrev, "__", focal_ent$entityName))
+
+  # Download it!
+  download.file(url = in_url, destfile =  in_file,
+    method = "curl", extra = paste0(' -A "', getOption("HTTPUserAgent"), '"'))
+}
 
 # End ----
