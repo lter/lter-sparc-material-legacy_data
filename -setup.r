680cd1415b95c124a0c19a88512e2956a1f1b794
njlyon0 <nicholasjlyon@gmail.com>
2026-09-08 10:42:27 -0400

    feat: add setup script

---
+++ -setup.r
@@ -0,0 +1,15 @@
+## -------------------------------------------- ##
+# General Setup Tasks
+## -------------------------------------------- ##
+# Purpose
+## Do generally-useful setup stuff (esp. if necessary for multiple downstream scripts)
+## !!! NOTE: this script should be `source`-able !!!
+
+# Clear environment/collect garbage
+rm(list = ls()); gc()
+
+# Make needed folder(s)
+dir.create(path = file.path("data"), showWarnings = FALSE)
+dir.create(path = file.path("graphs"), showWarnings = FALSE)
+
+# End ----
