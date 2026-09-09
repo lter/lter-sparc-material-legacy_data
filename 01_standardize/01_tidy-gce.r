27c580933a076c6bbaeac9159a553a5e312e9191
njlyon0 <nicholasjlyon@gmail.com>
2026-09-09 16:58:13 -0400

    feat (std): add first draft of GCE code

--- 01_standardize/01_tidy-gce.r
+++ 01_standardize/01_tidy-gce.r
@@ -14,8 +14,65 @@ source(file.path("-setup.r"))
 # Clear environment/collect garbage
 rm(list = ls()); gc()
 
-# Define 3-letter site abbreviation
-site_abbrev <- "GCE"
+## -------------------------------------------- ##
+# Tidy Marsh Data ----
+## -------------------------------------------- ##
 
+# Load the raw data
+gce_v01 <- data.frame("x" = readLines(file.path("data", "raw", "00_GCE__PLT-GCES-1609_Biomass_Stats.csv")))
+
+# Check structure
+dplyr::glimpse(gce_v01)
+
+# Get this into dataframe format
+gce_v02 <- gce_v01 %>%
+  dplyr::slice(6:nrow(.)) %>% 
+  tidyr::separate_wider_delim(cols = x, delim = ",",
+    names = tolower(c("Year", "Site", "Zone", "Plot", "Quadrat_Area", 
+    "Plot_Disturbance", "Percent_Calculated_Biomass", "Num_Plant_Biomass_m2", 
+    "Min_Plant_Biomass_m2", "Max_Plant_Biomass_m2", "Total_Plant_Biomass_m2", 
+    "Mean_Plant_Biomass_m2", "StdDev_Plant_Biomass_m2", "SE_Plant_Biomass_m2"))) %>% 
+  dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
+    .fns = as.numeric))
+
+# Check structure
+dplyr::glimpse(gce_v02)
+
+# Filter to only needed rows
+gce_v03 <- gce_v02 %>% 
+  dplyr::filter(quadrat_area == 0.25) %>% 
+  dplyr::filter(!is.na(total_plant_biomass_m2))
+
+# Check structure
+dplyr::glimpse(gce_v03)
+
+# Clarify disturbance column and summarize
+gce_v04 <- gce_v03 %>% 
+  dplyr::mutate(plot_disturbance = ifelse(plot_disturbance == 1,
+    yes = "yes", no = "no")) %>% 
+  dplyr::group_by(site, year, plot_disturbance) %>% 
+  dplyr::summarize(biomass.mean = mean(total_plant_biomass_m2, na.rm = TRUE),
+    biomass.sd = sd(total_plant_biomass_m2, na.rm = TRUE),
+    biomass.n = dplyr::n(),
+    biomass.se = (biomass.sd / sqrt(biomass.n)),
+    .groups = "drop") %>% 
+  dplyr::select(-biomass.sd, -biomass.n)
+
+# Check structure
+dplyr::glimpse(gce_v04)
+
+## -------------------------------------------- ##
+# Export ----
+## -------------------------------------------- ##
+
+# Make a final version of the data
+gce_v99 <- gce_v04
+
+# One last structure check
+dplyr::glimpse(gce_v99)
+
+# Export locally
+write.csv(gce_v99, row.names = FALSE, na = '',
+  file = file.path("data", "standard", "01_GCE_marsh-biomass.csv"))
 
 # End ----
