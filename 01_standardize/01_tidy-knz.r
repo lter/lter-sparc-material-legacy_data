aa640f28fc7e7280793153b0598465c5709b6473
njlyon0 <nicholasjlyon@gmail.com>
2026-09-09 11:38:35 -0400

    refactor (std): make KNZ code more efficienct and linear

--- 01_standardize/01_tidy-knz.r
+++ 01_standardize/01_tidy-knz.r
@@ -22,17 +22,15 @@ rm(list = ls()); gc()
 # Read in annual burn watershed data
 knz_1y <- read.csv(file.path("data", "raw", "00_KNZ__PAB011.csv")) %>% 
   janitor::clean_names() %>%  
-  dplyr::filter(watershed == "001d")%>%
-  dplyr::rename(year = recyear)
-
+  dplyr::mutate(source = "1 year")
+  
 # Check structure
 dplyr::glimpse(knz_1y)
 
 # Two year watershed
 knz_2y <- read.csv(file.path("data", "raw", "00_KNZ__PAB041.csv")) %>% 
   janitor::clean_names() %>%  
-  dplyr::filter(watershed == "002d") %>% 
-  dplyr::rename(year = recyear)
+  dplyr::mutate(source = "2 year")
 
 # Check structure
 dplyr::glimpse(knz_2y)
@@ -42,72 +40,75 @@ dplyr::glimpse(knz_2y)
 ## -------------------------------------------- ##
 
 # Read in burn year data
-knz_burns_v01 <- read.csv(file.path("data", "raw", "00_KNZ__KFH011.csv")) %>% 
+knz_years_burn <- read.csv(file.path("data", "raw", "00_KNZ__KFH011.csv")) %>% 
   janitor::clean_names() %>%  
   dplyr::filter(watershed == "2D")
 
 # Check structure
-dplyr::glimpse(knz_burns_v01)
+dplyr::glimpse(knz_years_burn)
 
 # Generate table of all years & identify burn vs. no burn
-knz_burns <- data.frame("year" = seq(from = 1978, to = max(c(knz_1y$year, knz_2y$year), na.rm = TRUE))) %>% 
-  dplyr::mutate(burn_year = ifelse(year %in% knz_burns_v01$year,
+knz_years_all <- data.frame("year" = seq(from = 1978, 
+    to = max(c(knz_1y$recyear, knz_2y$recyear), na.rm = TRUE))) %>% 
+  dplyr::mutate(burn_year = ifelse(year %in% knz_years_burn$year,
     yes = "yes", no = "no"))
 
 # Check structure
-dplyr::glimpse(knz_burns)
+dplyr::glimpse(knz_years_all)
 
-## -------------------------------------------- ##
-# Summarize Biomass Data ----
-## -------------------------------------------- ##
-
-# Get average grass cover for 1 year
-knz_1y_avg <- knz_1y %>% 
-  dplyr::group_by(year, soiltype, transect) %>% 
-  dplyr::summarize(lvgrass.mean = mean(lvgrass, na.rm = TRUE),
-    .groups = "drop") %>% 
-  dplyr::mutate(burn_cat = "burned")
-
-# Filter two-year data to only unburned years and summarize it too
-knz_2y_noburn_avg <- knz_2y %>% 
-  dplyr::filter(!year %in% knz_burns_v01$year) %>% 
-  dplyr::group_by(year, soiltype, transect) %>% 
-  dplyr::summarize(lvgrass.mean = mean(lvgrass, na.rm = TRUE),
-    .groups = "drop") %>% 
-  dplyr::mutate(burn_cat = "unburned")
-    
-# Check structure
-dplyr::glimpse(knz_2y_noburn_avg)
-
-## -------------------------------------------- ##
-# Filter Annual Data to Target Years ----
-## -------------------------------------------- ##
-
-# Extract unique years from the non-burned dataset
-(target_years <- unique(knz_2y_noburn_avg$year))
-
-# Filter annual dataset to only those years
-knz_1y_noburn_avg <- knz_1y_avg %>% 
- dplyr::filter(year %in% target_years)
-
-# Combine data
-knz_merge <- dplyr::bind_rows(knz_1y_noburn_avg, knz_2y_noburn_avg) %>% 
-  dplyr::filter(soiltype == "fl")
+# Identify unburned years
+knz_years_unburn <- knz_years_all %>% 
+  dplyr::filter(burn_year == "no")
 
 # Check structure
-dplyr::glimpse(knz_merge)
+dplyr::glimpse(knz_years_unburn)
+
+## -------------------------------------------- ##
+# Streamline Biomass Data ---
+## -------------------------------------------- ##
+
+# Combine & filter biomass
+knz_v01 <- dplyr::bind_rows(knz_1y, knz_2y) %>% 
+  dplyr::rename(year = recyear) %>% 
+  # Keep only one watershed for 1 year data
+  dplyr::filter((source == "1 year" & watershed == "001d") |
+    # Keep only one watershed for 2 year data but only in unburned years
+    (source == "2 year" & watershed == "002d")) %>% 
+  dplyr::filter(soiltype == "fl") %>% 
+  dplyr::filter(year %in% knz_years_unburn$year)
+
+# Check structure
+dplyr::glimpse(knz_v01)
+
+# Summarize biomass
+knz_v02 <- knz_v01 %>% 
+  dplyr::group_by(source, year, soiltype, transect) %>% 
+  dplyr::summarize(lvgrass.mean = mean(lvgrass, na.rm = TRUE),
+    .groups = "drop")
+
+# Check structure
+dplyr::glimpse(knz_v02)
+
+# Identify burn status
+knz_v03 <- knz_v02 %>% 
+  dplyr::mutate(burn_cat = ifelse(source == "1 year", 
+    yes = "burned", no = "unburned"))
+
+# Check structure
+dplyr::glimpse(knz_v03)
 
 ## -------------------------------------------- ##
 # Export ----
 ## -------------------------------------------- ##
 
 # Make a final version of the data
-
+knz_v99 <- knz_v03
 
 # One last structure check
-
+dplyr::glimpse(knz_v99)
 
 # Export locally
-
+write.csv(knz_v99, row.names = FALSE, na = '',
+  file = file.path("data", "standard", "01_KNZ.csv"))
 
 # End ----
