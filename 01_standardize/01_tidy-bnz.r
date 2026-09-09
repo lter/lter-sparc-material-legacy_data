9b6c0ce068d12d44bf19bc29fd8a5e7febe9e302
njlyon0 <nicholasjlyon@gmail.com>
2026-09-09 16:11:26 -0400

    feat (std): add first draft of BNZ code

--- 01_standardize/01_tidy-bnz.r
+++ 01_standardize/01_tidy-bnz.r
@@ -1,5 +1,5 @@
 ## -------------------------------------------- ##
-# Download Data - Bonanza Creek (BNZ)
+# Standardize Data - Bonanza Creek (BNZ)
 ## -------------------------------------------- ##
 # Purpose
 ## Get these data into a standard format with that of other sites
@@ -16,6 +16,58 @@ rm(list = ls()); gc()
 
 # Define 3-letter site abbreviation
 site_abbrev <- "BNZ"
+
+## -------------------------------------------- ##
+# Tidy Forest Fire Data ----
+## -------------------------------------------- ##
+
+# Load in the data file
+bnz_v01 <- read.csv(file = file.path("data", "raw", "00_BNZ__342_JFSP_sitedata_2011.txt"),
+    sep = "\t")
+
+# Check structure
+dplyr::glimpse(bnz_v01)
+
+names(bnz_v01)
+
+# Pare down the dataset to only columns that are needed
+bnz_v02 <- bnz_v01 %>% 
+  dplyr::rename(moisture = moist.2008,
+    elevation = elev,
+    Can.cons.percent = X.Can.cons,
+    Standing.percent = X.Standing_Num,
+    BS.density = BS.dens
+  ) %>% 
+  dplyr::select(burn, site, type, moisture, elevation, tree.sev.rank, 
+    Can.cons.percent, Standing.percent, CBI.Over, 
+    BS.density, BS.ba, BS.ntree, BS.nstand)
+  
+# What columns are lost/gained
+supportR::diff_check(old = names(bnz_v01), new = names(bnz_v02))
+
+# Check structure
+dplyr::glimpse(bnz_v02)
+
+# Filter as needed
+bnz_v03 <- bnz_v02 %>% 
+  dplyr::filter(type != "ext")
+
+# Check structure
+dplyr::glimpse(bnz_v03)
+
+## -------------------------------------------- ##
+# Export ----
+## -------------------------------------------- ##
+
+# Make a final version of the data
+bnz_v99 <- bnz_v03
+
+# One last structure check
+dplyr::glimpse(bnz_v99)
+
+# Export locally
+write.csv(bnz_v99, row.names = FALSE, na = '',
+  file = file.path("data", "standard", "01_BNZ_forest-fire.csv"))
 
 
 # End ----
