2f6068c0e64609ebcf607730375052926a457cab
njlyon0 <nicholasjlyon@gmail.com>
2026-09-09 11:14:35 -0400

    feat (setup): add new 'standard' subfolder of 'data'

--- -setup.r
+++ -setup.r
@@ -10,6 +10,7 @@ rm(list = ls()); gc()
 
 # Make needed folder(s)
 dir.create(path = file.path("data", "raw"), showWarnings = FALSE, recursive = TRUE)
+dir.create(path = file.path("data", "standard"), showWarnings = FALSE)
 dir.create(path = file.path("graphs"), showWarnings = FALSE)
 
 # End ----
