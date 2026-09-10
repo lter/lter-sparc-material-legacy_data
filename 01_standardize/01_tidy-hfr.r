8db80015487ed3473076eb8ff434e581480f985a
njlyon0 <nicholasjlyon@gmail.com>
2026-09-10 11:12:18 -0400

    feat (std): add complete HFR code

--- 01_standardize/01_tidy-hfr.r
+++ 01_standardize/01_tidy-hfr.r
@@ -19,42 +19,17 @@ source(file.path("-setup.r"))
 rm(list = ls()); gc()
 
 ## -------------------------------------------- ##
-# Tidy Predictor Data ----
+# Tidy Hemlock Data ----
 ## -------------------------------------------- ##
 
-# Load predictor variables
-hfr_pred_v01 <- read.csv(file.path("data", "from-drive", "DeadByPlot.csv"))
+# Load relevant data
+hfr_v01 <- read.csv(file.path("data", "from-drive", "TreeSapDensSpecies.csv"))
 
 # Check structure
-dplyr::glimpse(hfr_pred_v01)
-
-# Summarize to get unique rows
-hfr_pred_v02 <- hfr_pred_v01 %>% 
-  dplyr::group_by(plot, trt, group, block, year) %>% 
-  dplyr::summarize(volm3ha.mean = mean(volm3ha, na.rm = TRUE),
-    massgm2.mean = mean(massgm2, na.rm = TRUE),
-    .groups = "drop")
-
-# Reshape wider
-hfr_pred_v03 <- hfr_pred_v02 %>% 
-  tidyr::pivot_wider(names_from = group, 
-    values_from = c(volm3ha.mean, massgm2.mean))
-
-# Check structure
-dplyr::glimpse(hfr_pred_v03)
-
-## -------------------------------------------- ##
-# Tidy Response Data ----
-## -------------------------------------------- ##
-
-# Load response data
-hfr_resp_v01 <- read.csv(file.path("data", "from-drive", "TreeSapDensSpecies.csv"))
-
-# Check structure
-dplyr::glimpse(hfr_resp_v01)
+dplyr::glimpse(hfr_v01)
 
 # Simplify some categorical columns in data
-hfr_resp_v02 <- hfr_resp_v01 %>% 
+hfr_v02 <- hfr_v01 %>% 
   dplyr::mutate(
     species = ifelse(nchar(species) == 0 | is.na(species),
       yes = "none", no = species),
@@ -64,49 +39,44 @@ hfr_resp_v02 <- hfr_resp_v01 %>%
       TRUE ~ "other"))
 
 # Check structure
-dplyr::glimpse(hfr_resp_v02)
+dplyr::glimpse(hfr_v02)
+
+# Do some needed filtering
+hfr_v03 <- hfr_v02 %>% 
+  dplyr::filter(trt %in% c("girdled", "logged")) %>% 
+  dplyr::filter(stratum == "Sapling") %>% 
+  dplyr::filter(year > 2004)
+
+# Check structure
+dplyr::glimpse(hfr_v03)
 
 # Summarize within needed columns
-hfr_resp_v03 <- hfr_resp_v02 %>% 
-  dplyr::group_by(plot, trt, block, year, spgroup, stratum) %>%
+hfr_v04 <- hfr_v03 %>% 
+  dplyr::group_by(plot, trt, block, year, spgroup) %>%
   dplyr::summarize(dens_ha = sum(dens.ha, na.rm = TRUE),
-    biomass_gm2 = sum(biomass.gm2, na.rm = TRUE),
-    .groups = "drop")
+    .groups = "drop") %>% 
+  dplyr::group_by(plot, trt, block, year) %>% 
+  dplyr::mutate(dens_ha_total = sum(dens_ha, na.rm = TRUE)) %>% 
+  dplyr::ungroup()
 
 # Check structure
-dplyr::glimpse(hfr_resp_v03)
+dplyr::glimpse(hfr_v04)
 
-# Reshape and calculate needed metrics
-hfr_resp_v04 <- hfr_resp_v03 %>% 
-  tidyr::pivot_wider(names_from = spgroup, 
-    values_from = c(dens_ha, biomass_gm2),
-    values_fill = 0) %>% 
-  dplyr::mutate(
-    dens_ha_total = dens_ha_hemlock + dens_ha_birch + dens_ha_other,
-    biomass_gm2_total = biomass_gm2_hemlock + biomass_gm2_birch + biomass_gm2_other 
-  )
+# Filter to only hemlock density data
+hfr_v05 <- hfr_v04 %>% 
+  dplyr::filter(spgroup == "hemlock") %>% 
+  dplyr::rename(dens_ha_hemlock = dens_ha) %>% 
+  dplyr::select(-spgroup)
 
 # Check structure
-dplyr::glimpse(hfr_resp_v04)
-
-## -------------------------------------------- ##
-# Combine & Filter Data ----
-## -------------------------------------------- ##
-
-# Join the two data files
-hfr_v01 <- hfr_resp_v04 %>% 
-  dplyr::inner_join(x = ., y = hfr_pred_v02,
-    by = c("plot", "year", "trt", "block"))
-
-# Check structure
-dplyr::glimpse(hfr_v01)
+dplyr::glimpse(hfr_v05)
 
 ## -------------------------------------------- ##
 # Export ----
 ## -------------------------------------------- ##
 
 # Make a final version of the data
-hfr_v99 <- hfr_v01
+hfr_v99 <- hfr_v05
 
 # One last structure check
 dplyr::glimpse(hfr_v99)
